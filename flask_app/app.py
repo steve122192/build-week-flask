8ff0312002791a3d4ba5549920099be441fa87aa
Steven Reiss <sgreiss92@gmail.com>
2020-03-05 20:25:42 -0500

    final

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,11 +3,12 @@ from flask import Flask, request, render_template, jsonify, flash, redirect
 from flask_migrate import Migrate
 from flask_app.models import db, migrate, Song
 import json
-#from review.game_service import api
 import joblib
 import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+
+# Import Predictive Model
 filename = 'Nearest_Neighbors_2.sav'
 nn = joblib.load(filename)
 
@@ -16,8 +17,8 @@ nn = joblib.load(filename)
 def create_app():
     app = Flask(__name__)
 
+    # Connect App to DB
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://tabtadgh:qLJzcc_KrEVBeCdqnhlKayggOHmHolNX@drona.db.elephantsql.com:5432/tabtadgh'
-    #have the db know about the app
     db.init_app(app)
     migrate.init_app(app, db)
 
@@ -25,12 +26,11 @@ def create_app():
     def root():
         return ('Hello World')
 
-
+# Decided to use DataFrame Instead of DB
     # @app.route("/<id>")
     # def get_song_data(id=None):
     #     try:
     #         x = Song.query.filter(Song.id == id).all()
-            
     #         x = x[0].__dict__
     #         del x["_sa_instance_state"]
     #         del x["artist_name"]
@@ -45,18 +45,16 @@ def create_app():
     #         predictions = nn.kneighbors(x)[1][0]
     #         predictions = predictions.tolist()
     #         predictions = Song.query.filter(Song.id.in_(predictions))
-
-            
-        
-            
-            
-            
     #         return jsonify(x)
     #     except:
     #         return jsonify({"message": "Song Not Found!"})
 
     @app.route("/predict/<id>")
     def get_predictions(id=None):
+        '''
+        Takes a spotify 'track_id', pulls the related song out of a Dataframe, 
+        runs it through a predictive model, and returns suggested songs in JSON format
+        '''
         try:
             df = pd.read_csv('http://www.zernach.com/wp-content/uploads/2020/02/SpotifyAudioFeaturesApril2019.csv')
             target   = 'track_id'
@@ -80,7 +78,6 @@ def create_app():
             predictions      = nn.kneighbors(track)[1][0]
             df_top_similar   = df.iloc[predictions]
             json_top_similar = df_top_similar.to_json(orient='records')
-            #json_top_similar = json.loads(df_top_similar.to_json())
             
             return json_top_similar
         except:
@@ -88,6 +85,9 @@ def create_app():
 
     @app.route("/data/<id>")
     def get_data(id=None):
+        '''
+        returns JSON song data for given Spotify 'track_id'
+        '''
         try:
             spotify = spotipy.Spotify(
                 client_credentials_manager=SpotifyClientCredentials(
@@ -103,8 +103,4 @@ def create_app():
         except:
             return jsonify({"message": "Song Not Found!"})  
 
-
-
-    
-    
     return app
